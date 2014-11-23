e415c89c8c1f79803fa45a118a247bfc3423a633
superkevingit <695851795@qq.com>
2014-11-23 09:26:05 +0800

    登陆时进行数据库查询

--- login.php
+++ login.php
@@ -1,9 +1,12 @@
+<meta charset="UTF-8">
 <?php
 session_start();
+//error_reporting(0);
 ?>
 <?php
 include("conn.php"); 
-function redirect(){
+function redirect()
+{
 	$url="index.php";
 	echo "<script language='javascript' type='text/javascript'>";
 	echo "window.location.href='$url'";
@@ -15,13 +18,13 @@ if (isset ($_SESSION['un']))
 }
 else
 {
-	$un=$_POST['username'] ;    //获取参数
-	$pw=$_POST['password'] ;//验证管理员名称和密码是否正确,这里采用直接验证,没有连接数据库	
-	if ($un=="username" and $pw=="password")
+	$un=$_POST['username'];    
+	$pw=md5($_POST['password']); 
+	$check_query=mysql_query("SELECT * FROM `editors` WHERE `un`='$un' and `pw`='$pw'"); 	
+	if ($result= mysql_fetch_array($check_query))
 	{
-		$_SESSION['un']=$un;        //注册新的变量,保存当前会话的昵称
-		$_SESSION['pw']=$pw;
-		 //登录成功回到主页面
+		$_SESSION['un']=$un;        
+		$_SESSION['pw']=$pw;       
 		echo "<script>alert('登陆成功');</script>";
 		redirect();
 	}
@@ -30,5 +33,4 @@ else
 		echo "<script>alert('登陆信息不正确');</script>";
 	}
 }
-
 ?>
