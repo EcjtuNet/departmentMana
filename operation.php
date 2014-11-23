33bb0d7071484af741faf154773f36c8f3bda188
superkevingit <695851795@qq.com>
2014-11-23 08:47:33 +0800

    添加统计操作
    增加数据库事务，自定义函数部分user传不进来，导致插入数据库时统计（statistic）表单无名字显示，总表（message）无人加减分

--- operation.php
+++ operation.php
@@ -1,11 +1,13 @@
+<meta charset="UTF-8">
 <?php
    session_start ();
-   if(!isset($_SESSION['un']))
+   //error_reporting(0);
+   /*if(!isset($_SESSION['un']))
 	{
 		echo "请登录!";
 	}
 	else
-	{ 
+	{ */
 	   if(isset($_POST['submit']))
 	   {
 		   $operation=$_POST['submit'];
@@ -22,14 +24,70 @@
 	   {
 		   $word=$_POST['word'];
 	   }
-/* -------------------------------------------分割线-----------------------------------------------------------*/
-
+	   if(isset($_POST['statistic']))
+	   {
+	   	   $statistic=$_POST['statistic'];	
+	   }
+/* -------------------------------------------分割线--自定义函数---------------------------------------------------------*/
+	   function add($type,$num,$operation)
+               {
+	               include("conn.php");
+				   mysql_query("BEGIN");
+				   //操作message表
+				   $query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
+				   $row=mysql_fetch_array($query);
+				   echo $user;
+				   $cal=floatval($row[$type])+ 1;
+				   $mul=floatval($row['total'])- $num;
+				   $res1=mysql_query("UPDATE `message` SET `$type`='$cal',`total`='$mul' WHERE `user`='$user'");
+				   //操作statistic表
+		           $res2=mysql_query("INSERT INTO `statistic`(`id`,`name`,`operation`,`time`) VALUES ('','$user','$operation',unix_timestamp())");
+				   //事务判断
+				   if($res1 && $res2)
+				   {
+					   	mysql_query("COMMIT");
+					   	$row=mysql_fetch_array($query);
+			            echo json_encode($row[$type]);
+				   }
+				   else
+				   {
+		                mysql_query("ROLLBACK");
+		                echo "try agian!";
+				   }
+				   mysql_query("END");
+			}
+	   function del($type,$num,$operation)
+               {
+	               include("conn.php");
+				   mysql_query("BEGIN");
+				   //操作message表
+				   $query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
+				   $row=mysql_fetch_array($query);
+				   $cal=floatval($row[$type])- 1;
+				   $mul=floatval($row['total'])+ $num;
+				   $res1=mysql_query("UPDATE `message` SET `$type`='$cal',`total`='$mul' WHERE `user`='$user'");
+				   //操作statistic表
+		           $res2=mysql_query("INSERT INTO `statistic`(`id`, `name`, `operation`,`time`) VALUES ('','$user','$operation',unix_timestamp())");
+				   //事务判断
+				   if($res1 && $res2)
+				   {
+					   	mysql_query("COMMIT");
+					   	$row=mysql_fetch_array($query);
+			            echo json_encode($row[$type]);
+				   }
+				   else
+				   {
+		                mysql_query("ROLLBACK");
+		                echo "try agian!";
+				   }
+				   mysql_query("END");
+			}
+/* -------------------------------------------分割线--添加用户-----------------------------------------------------------*/
 	   //添加一个新用户
-	   if($operation=='add')
+	    if($operation=='add')
 	   {
 		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $result= mysql_query($sql);
+		   $result= mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
 		   $row=mysql_fetch_array($result);
 		   if($row)
 			{
@@ -37,13 +95,12 @@
 		    }
            else
 		   {
-			   $add="INSERT INTO `message`(`id`, `user`, `uarrive`, `vacate`, `late`, `arrive`,`total`) VALUES ('','$user','0','0','0','0','0')";
-			   mysql_query($add);
-			   echo "OKadd";
-		   }
-	   }
- /* -------------------------------------------分割线-----------------------------------------------------------*/
-	   //删除一个用户
+			   mysql_query("INSERT INTO `message`(`id`, `user`, `uarrive`, `vacate`, `late`,`accident`,`total`) VALUES ('','$user','0','0','0','0','0')");
+		       echo "okadd";
+	       }
+	    }
+/* -------------------------------------------分割线--删除用户-----------------------------------------------------------*/
+	  //删除一个用户
 	   elseif($operation=='del')
 	   {
 		   include("conn.php");
@@ -51,146 +108,130 @@
 		   mysql_query($del); 
 		   echo "OKdel";
 	   }
-/* -------------------------------------------分割线-----------------------------------------------------------*/
-       //更改积分
+/* -------------------------------------------分割线--更改工作点---------------------------------------------------------*/
+       //更改工作点
 	   elseif($operation=='update')
 	   {
 		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
+		   mysql_query("BEGIN");
+		   //操作message表
+		   $query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
 		   $row=mysql_fetch_array($query);
-		   $value=intval($value);
-		   $cal=intval($row['total'])+$value;
-		   $update="UPDATE `message` SET `total`='$cal' WHERE `user`='$user'";
-		   mysql_query($update);
-		   $sql="INSERT INTO `history`(`name`,`history`,`time`,`value`) VALUES ('$user','$word',now(),'$value')";
-		   mysql_query($sql);
-		   echo "OKupdate";
+		   $value=floatval($value);
+		   $cal1=floatval($row['total'])+$value;
+		   $res1=mysql_query("UPDATE `message` SET `total`='$cal1' WHERE `user`='$user'");
+		   //操作history表
+		   $un=$_SESSION['un'];
+		   $res2=mysql_query("INSERT INTO `history`(`name`,`history`,`time`,`value`,`editor`) VALUES ('$user','$word',unix_timestamp(),'$value','$un')");
+		   //事务判断
+		   if($res1 && $res2)
+		   {
+			   	  mysql_query("COMMIT");
+			   	  echo "OKupdate";
+		   }
+		   else
+		   {
+               mysql_query("ROLLBACK");
+               echo "try agian!";
+		   }
+		   mysql_query("END");
 	   }
-/* -------------------------------------------分割线-----------------------------------------------------------*/
-	   //加减缺勤分
-	   elseif($operation=='uarriveadd')
+/* -------------------------------------------分割线--微调工作点---------------------------------------------------------*/
+	   //调整工作点
+	   elseif($operation=='updateadd')
 	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['uarrive'])+ 1;
-		   $update="UPDATE `message` SET `uarrive`='$cal' WHERE `user`='$user'";
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['uarrive']);
+	   	    include("conn.php");
+	   	    mysql_query("BEGIN");
+			//操作message表
+			$query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
+			$row=mysql_fetch_array($query);
+			$cal=floatval($row['total'])+ 0.1;
+			$res=mysql_query("UPDATE `message` SET `total`='$cal' WHERE `user`='$user'");
+		    //操作statistic表
+		    $statistic=mysql_query("INSERT INTO `statistic`(`id`,`name`,`operation`,`time`) VALUES ('','$user','8',unix_timestamp())");
+		    if($res && $statistic)
+		    {
+		        mysql_query("COMMIT");
+			    $query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
+			    $row=mysql_fetch_array($query);
+			    echo json_encode($row['total']);
+		    }
+		    else
+		    {
+			    mysql_query("ROLLBACK");
+			    echo "try again!";
+		    }
+		    mysql_query("END");
 	   }
+	   elseif($operation=='updatedel')
+	   {
+	   	    include("conn.php");
+	   	    mysql_query("BEGIN");
+	   	    //操作message表
+			$query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
+			$row=mysql_fetch_array($query);
+			$cal=floatval($row['total'])- 0.1;
+		    $res=mysql_query("UPDATE `message` SET `total`='$cal' WHERE `user`='$user'");
+		    //操作statistic表
+		    $statistic=mysql_query("INSERT INTO `statistic`(`id`,`name`,`operation`,`time`) VALUES ('','$user','9',unix_timestamp())");
+		    if($res && $statistic)
+		    {
+		        mysql_query("COMMIT");
+			    $query=mysql_query("SELECT * FROM `message` WHERE `user`='$user'");
+			    $row=mysql_fetch_array($query);
+			    echo json_encode($row['total']);
+		    }
+		    else
+		    {
+			    mysql_query("ROLLBACK");
+			    echo "try again!";
+		    }
+		    mysql_query("END");
+	   }
+/* -------------------------------------------分割线--旷到---------------------------------------------------------------*/
+	   //加减旷到分
+	   elseif($operation=='uarriveadd')
+		{
+		 	  add($type='uarrive',$num='floatval("2")',$operation="0"); 
+	    }
 	   elseif($operation=='uarrivedel')
 	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['uarrive'])- 1;
-		   $update="UPDATE `message` SET `uarrive`='$cal' WHERE `user`='$user'";
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['uarrive']);
+			  del($type='uarrive',$num='floatval("2")',$operation="1");
 	   }
-/* -------------------------------------------分割线-----------------------------------------------------------*/
+/* -------------------------------------------分割线--请假---------------------------------------------------------------*/
 	   //加减请假分
 	   elseif($operation=='vacateadd')
 	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['vacate'])+ 1;
-		   $update="UPDATE `message` SET `vacate`='$cal' WHERE `user`='$user'" ;
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['vacate']);
+		      add($type='vacate',$num='intval("1")',$operation="2");
 	   }
 	   elseif($operation=='vacatedel')
 	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['vacate'])- 1;
-		   $update="UPDATE `message` SET `vacate`='$cal' WHERE `user`='$user'"  ;
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['vacate']);
+		      del($type='vacate',$num='intval("1")',$operation="3");
 	   }
- /* -------------------------------------------分割线-----------------------------------------------------------*/
+/* -------------------------------------------分割线--迟到---------------------------------------------------------------*/
 	   //加减迟到分
 	   elseif($operation=='lateadd')
 	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['late'])+ 1;
-		   $update="UPDATE `message` SET `late`='$cal' WHERE `user`='$user'" ;
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['late']);
+		   add($type='late',$num='floatval("0.5")',$operation="4");
 	   }
 	   elseif($operation=='latedel')
 	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['late'])- 1;
-		   $update="UPDATE `message` SET `late`='$cal' WHERE `user`='$user'" ;
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['late']);		   
+		   del($type='late',$num='floatval("1")',$operation="5");
+	   }
+/* -------------------------------------------分割线--新闻事故-----------------------------------------------------------*/
+       //加减新闻事故
+       elseif ($operation=='accident') 
+       {
+       	    add($type='accident',$num='floatval("0")',$operation="6");
+       }
+       elseif($operation=='latedel')
+	   {
+		    del($type='accident',$num='floatval("0")',$operation="7");
 	   }
 /* -------------------------------------------分割线-----------------------------------------------------------*/
-	   //加减考勤分
-	   elseif($operation=='arriveadd')
-	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['arrive'])+ 1;
-		   $update="UPDATE `message` SET `arrive`='$cal' WHERE `user`='$user'";
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['arrive']);		   
-	   }
-	   elseif($operation=='arrivedel')
-	   {
-		   include("conn.php");
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   $cal=intval($row['arrive'])- 1;
-		   $update="UPDATE `message` SET `arrive`='$cal' WHERE `user`='$user'";
-		   mysql_query($update);
-		   $sql="SELECT * FROM `message` WHERE `user`='$user'";
-		   $query=mysql_query($sql);
-		   $row=mysql_fetch_array($query);
-		   echo json_encode($row['arrive']);	
-		   
-	   }
 	   else
 	   {
 		   echo "error";
 	   }
-   }
+   //}
 ?>
